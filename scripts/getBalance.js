c3f8331802b5d3af0c4dfe843e75dbf967b20087
Sushant Raj <sushantraj893@gmail.com>
2024-08-31 00:28:17 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/Gurdwara.sol/Gurdwara.json");
+const tokenContractJSON = require("../artifacts/contracts/cu.sol/cu.json");
 
-const tokenAddress = "0x78EDe6348F07E5499340b709d4e325963Dc651d5"; // place your erc20 contract address here
+const tokenAddress = "0x643E0F9aBa4CFB2b26e9a18d84BED0cbD88F03e6"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0x746D818F4f239e57986d810a4d55526526414C5D"; // place your public address for your wallet here
+const walletAddress = "0x28D1759B13CD4fA182d18E2Be3D4A5DdBEE6920F"; // place your public address for your wallet here
 
 async function main() {
 
